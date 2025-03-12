87b72e25186a5d5767783cfe42ba08bc9f5e2293
Iliaz Mamedov <ilzmamedov@gmail.com>
2025-03-12 16:31:54 +0300

    fix: minor fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,9 +2,13 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
+    firstName: { type: String, reqired: true },
+    lastName: { type: String, reqired: true },
     fullName: { type: String, reqired: true },
     imageUrl: { type: String, reqired: true },
     clerkId: { type: String, reqired: true, unique: true },
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   },
   { timestamps: true },
 );
