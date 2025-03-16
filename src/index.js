fe6c7f9b72c58e5c429663c7535d6f35c526aee5
Iliaz Mamedov <ilzmamedov@gmail.com>
2025-03-16 23:23:34 +0300

    preparations for deployment

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ import albumRoutes from './routes/album.route.js';
 import statRoutes from './routes/stat.route.js';
 import { connectDB } from './lib/db.js';
 import cors from 'cors';
+import fs from 'fs';
 import { createServer } from 'http';
-
+import cron from 'node-cron';
 dotenv.config();
 
 const __dirname = path.resolve();
@@ -40,10 +41,25 @@ app.use(
     tempFileDir: path.join(__dirname, 'tmp'),
     createParentPath: true,
     limits: {
-      fileSize: 10 * 10 * 1024 * 1024, // 10MB
+      fileSize: 10 * 10 * 1024 * 1024, // 100MB
     },
   }),
 );
+
+const tempDir = path.join(process.cwd(), 'tmp');
+cron.schedule('0 * * * *', () => {
+  if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log('error in tmpDir removing ', err);
+        return;
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {});
+      }
+    });
+  }
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
@@ -52,6 +68,12 @@ app.use('/api/songs', songRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/stats', statRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+  });
+}
 app.use((error, req, res, next) => {
   res.status(500).json({
     message:
